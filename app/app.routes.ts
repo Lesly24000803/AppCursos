b5e28407a4e18eb7386440eca4f1cf5102089ecc
lesly24000803 <Leslymejia095@gmail.com>
2025-07-14 18:53:15 -0600

    Corrijo prerender dinámico para detalleCurso

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,20 +5,21 @@ import { NotFound } from './core/not-found/not-found';
 import { DetalleCurso } from './pages/cursos/detalle-curso/detalle-curso';
 
 export const routes: Routes = [
-    {
-        path: '', component: Inicio
-    },
-    {
-        path: 'inicio', component: Inicio
-   },
-    {
-        path: 'cursos', component: Cursos
-    },
-    {
-        path: 'detalleCurso/:id', component: DetalleCurso
-    },
-    {
-        path: '**', component: NotFound 
-    }
-
+  {
+    path: '', component: Inicio
+  },
+  {
+    path: 'inicio', component: Inicio
+  },
+  {
+    path: 'cursos', component: Cursos
+  },
+  {
+    /* path: 'detalleCurso/:id',
+    LoadComponent: () => import('./pages/cursos/detalle-curso/detalle-curso').then(m => m.DetalleCurso) */
+    path: 'cursos', component: Cursos
+  },
+  {
+    path: '**', component: NotFound
+  }
 ];
